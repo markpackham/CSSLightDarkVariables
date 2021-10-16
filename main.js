d486d4baeee4f2b768bb9b4820e6de721776fb4f
mark <markpackham1@gmail.com>
2021-10-16 17:44:37 +0100

    get site current theme
    https://youtu.be/uO3RF_67re8?t=532

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-import './style.css'
+import "./style.css";
+
+const themeBtn = document.querySelector("theme");
+
+// Figure out system light/dark default
+function getCurrentTheme() {
+  let theme = window.matchMedia("(prefers-colos-scheme: dark)").matches
+    ? "dark"
+    : "light";
+  return theme;
+}
